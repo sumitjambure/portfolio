f93138cf4d761e19986611bdb4b30a53f6e09180
sumitjambure <65492065+sumitjambure@users.noreply.github.com>
2024-09-03 23:36:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">import Navbar from "./Navbar";
+import About from "./About";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Appreciations from "./Appreciations";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Appreciations />
+      <Contact />
+      <Footer />
+    </>
+
+  );
+}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
