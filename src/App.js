f7be8ceafc5ebfa2e067ce994c179e3c0f76a8ae
sumitjambure <65492065+sumitjambure@users.noreply.github.com>
2024-09-04 10:01:57 +0530

    Update App.js
    updating the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
@@ -28,22 +23,5 @@ export default function App() {
   );
 }
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 export default App;
